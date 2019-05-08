7a1b7fdbc99b3ad538262ca94694d7be07ada3d6
Ken-Mens <kenneth.mensah@gmail.com>
2019-05-08 17:49:38 +0000

    fixed syntax

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     elif (len(sys.argv) == 2):
         print("{}".format("1 argument:"))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1), end="\n")
+        print("{} arguments:".format(len(sys.argv) - 1))
     if (len(sys.argv) > 1):
         for og in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]), end="\n")
+            print("{}: {}".format(i, sys.argv[i]))
             i += 1
