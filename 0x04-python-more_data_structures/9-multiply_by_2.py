c3b27f62169022f4d4186ec69fce8faeb7751600
Ken-Mens <kenneth.mensah@gmail.com>
2019-05-10 16:10:25 +0000

    fixed syntax

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):
-    new_dict = {}
-    for i in my_dict.items():
-        new_dict[i] = i * 2
-    return new_dict
+def multiply_by_2(a_dictionary):
+    a_dictionary = {}
+    for i in a_dictionary.items():
+        a_dictionary[i] = i * 2
+    return a_dictionary
