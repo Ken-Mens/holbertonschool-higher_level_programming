e85f627384e0ebab6c22bd9e4e55b422961afc45
Ken-Mens <kenneth.mensah@gmail.com>
2019-05-22 04:29:04 +0000

    write a lass Square that defines a square by my first square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class that defines a square"""
+
+
+class Square:
+    """Creates Square class"""
+
+    def __init__(self, size):
+        """Initializes Square with size & self"""
+        self.__size = size
