b822fad9a51945c439ab2e777acc41f9dce59e3a
Ken-Mens <kenneth.mensah@gmail.com>
2019-06-07 03:14:32 +0000

    fixed lines

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
+    lin = 0
     with open(filename, encoding="UTF-8") as foo:
-        for x, y in enumerate(foo):
-            pass
-    return (x + 1)
+        for line in foo:
+            lin += 1
+    return lin
