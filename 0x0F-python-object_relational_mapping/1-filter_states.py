dd9bd1640b4773da9244b56015ba56913cddc1b1
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-10 20:02:46 +0000

    filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 0-select_states.py: lists all states from database hbtn_0e_0_usa
+
+if __name__ == "__main__":
+
+    from sys import argv
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    import MySQLdb
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=username, passwd=password, db=database)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER by id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print (row)
