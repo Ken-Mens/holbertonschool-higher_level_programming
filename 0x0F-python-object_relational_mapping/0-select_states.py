d5642383f15aa3b8bcffc3dc636ded8d74ddcac4
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-10 20:00:03 +0000

    add 0 task get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,23 +4,19 @@
 """
 if __name__ == "__main__":
 
-    # get arguments
     from sys import argv
     username = argv[1]
     password = argv[2]
     database = argv[3]
 
-    # connect to database
     import MySQLdb
     db = MySQLdb.connect(host='localhost', port=3306,
-                         user=username, passwd=password, db=database)
-    # set our cursor
+                         user=username, passwd=password,
+                         db=database)
     cur = db.cursor()
 
-    # execute the commmand
     cur.execute('SELECT * FROM states ORDER by id ASC')
 
-    # print output
     rows = cur.fetchall()
     for row in rows:
         print (row)
