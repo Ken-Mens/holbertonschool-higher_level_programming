41df0abe91f00fd2980758933522a77018f7db5f
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-11 19:46:22 +0000

    fixed syntax

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # script that will list all states from databases
-""" 0-select_states list all states from database
-"""
 
 
 if __name__ == "__main__":
+    """ 0-select_states list all states from database
+    """
     import MySQLdb
     from sys import argv
 
