8b820407e6d57e38c660f3768c08858c473a0b15
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-11 19:53:09 +0000

    all states via Alchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     from model_state import Base, State
     from sqlalchemy import create_engine
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (argv[1], argv[2], argv[3], pool_pre_ping=True))
+                        (argv[1], argv[2], argv[3], pool_pre_ping=True))
 
     from sqlalchemy.orm import sessionmaker
     Session = sessionmaker(bind=eng)
