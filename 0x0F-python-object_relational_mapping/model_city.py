792ac8b7566f7a06dcbfe1e39918254680a693df
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-11 22:21:44 +0000

    changed relationship in city class

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,6 +4,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
+from sqlalchemy.orm import relationship
 
 
 class City(Base):
@@ -12,3 +13,4 @@ class City(Base):
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State")
