665d1574d73dd4a7e8bfa4cd9cbbcb2ecda0ec7b
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-11 22:00:46 +0000

    model city inheriting base

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # model_city.py: contains class definition of a City
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
+from model_state import Base
 
 class City(Base):
     """ City Class """
     __tablename__ = 'cities'
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey(State.id))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
