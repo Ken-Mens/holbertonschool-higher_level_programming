e0c03b8ad3bb52094cb3166c0f9d0838d5f235dd
Ken-Mens <kenneth.mensah@gmail.com>
2019-08-11 22:39:38 +0000

    fixed syntax

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,6 +10,6 @@ from sqlalchemy.orm import relationship
 class City(Base):
     """ City Class """
     __tablename__ = 'cities'
-    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
