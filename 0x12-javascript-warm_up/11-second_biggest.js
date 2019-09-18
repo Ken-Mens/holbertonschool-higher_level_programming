8482123c0eb2b9c0dba5baf44b4e0b8a8edcf01d
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-18 00:13:08 +0000

    script that searches for the second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+if (process.argv.length <= 3 || process.argv.length === 1) {
   console.log(0);
 } else {
   console.log(process.argv.sort().reverse()[1]);
