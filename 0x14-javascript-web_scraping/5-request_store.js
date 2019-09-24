6bcba6889f0ca02c2959f7340a6009d46c499949
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-24 22:38:46 +0000

    add request store javascript task

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const av = process.argv[2];
+const inp = process.argv[3];
+request.get(av, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  fs.writeFile(inp, body, (err) => {
+    if (err) console.log(err);
+  });
+});
