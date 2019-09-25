afe2b12d3cc60e42d055acbb3b99c90b7d501ebe
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-25 01:27:33 +0000

    changed wedge antilles task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const check = 'https://swapi.co/api/people/18/';
 const request = require('request');
 let counter = 0;
 request.get(process.argv[2], (err, res, body) => {
@@ -8,7 +7,7 @@ request.get(process.argv[2], (err, res, body) => {
   } else {
     const resp = JSON.parse(body);
     for (const x in resp.results) {
-      if (resp.results[x].characters.includes(check)) { counter++; }
+      if (resp.results[x].characters.includes('https://swapi.co/api/people/18/')) { counter++; }
     }
     console.log(counter);
   }
