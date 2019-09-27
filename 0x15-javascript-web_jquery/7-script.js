08e425ff9e2cd69a89948dd9d5a531fc034c2eb9
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-27 02:43:51 +0000

    changes api swapi

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,4 @@
+// Fetches and replaces the name of API data and replaces name
+$.get('https://swapi.co/api/people/5/?format=json', function (data, stat) {
+    $('div#character').text(data.name);
+});
